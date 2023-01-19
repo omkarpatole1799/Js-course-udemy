e7dfbc32f9e3848efe1b639b5bc3421b5cf4fcc2
Omkar Patole <omkarpatole1799@gmail.com>
2023-01-18 19:25:30 -0800

    init

---
+++ script.js
@@ -0,0 +1,49 @@
+// alert("hi");
+console.log("hi")
+
+
+//LECTURE: Values and Variables
+// const country = "India";
+// const continent = "Asia";
+// const population = 1.4;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+// coding challange 1 bmi calculator
+// case 1
+const markWeight1 = 78
+const markHeigh1 = 1.69
+
+const bmiMark1 = markWeight1 / (markHeigh1**2)
+console.log(bmiMark1)
+
+const jhonWeight1 = 92
+const jhonHeight1 = 1.95
+
+const bmiJhon1 = jhonWeight1 / (jhonHeight1**2)
+console.log(bmiJhon1)
+
+// checking if mark has higher bmi
+
+const markHeigherBmi1 = bmiMark1 > bmiJhon1
+console.log(markHeigherBmi1)
+
+// case 2
+const markWeight2 = 95
+const markHeigh2 = 1.88
+
+const markBmi2 = markWeight2 / (markHeigh2**2)
+console.log(markBmi2)
+
+const jhonWeight2 = 85
+const jhonHeight2 = 1.76
+
+const jhonBmi2 = jhonWeight2 / (jhonHeight2**2)
+console.log(jhonBmi2)
+
+//check if mark has heigher bmi
+const markHeigherBmi2 = markBmi2>jhonBmi2
+console.log(markHeigherBmi2)
