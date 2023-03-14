209ab3136a3ff5ec7f879a24479b9e4c65f52e59
Omkar Patole <omkarpatole1799@gmail.com>
2023-03-14 14:56:32 +0530

    async await function

--- Async Js/index.js
+++ Async Js/index.js
@@ -101,8 +101,121 @@ const getCountryData = function(country){
 //         .catch(err=>renderError(`something went wrong ${err.message}`))
 // }
 
-btn.addEventListener('click',()=>{
-    getCountryData('India')
-    // getCountryData('portugal')
-    // getCountryData('usa')
-})
+// btn.addEventListener('click',()=>{
+//     getCountryData('India')
+//     // getCountryData('portugal')
+//     // getCountryData('usa')
+// })
+
+// micro tasks
+/*
+- they have more priority than normal tasks
+- suppose we have timer function and micro tasks then micro task will execute first then timer task will execute
+*/
+// these are regular tasks
+// setTimeout(()=>console.log('0 sec timer'),0)
+
+// these are micro tasks
+// Promise.resolve('resolved promise 1').then(res=>console.log(res))
+// Promise.resolve('resolved promise 2').then(res=>{
+//     for(let i=0; i<100000; i++){}
+//     console.log(res)
+// })
+
+// // lottery ticket example
+// // create manual promise
+
+// const lotteryTicket = new Promise((resolve,reject)=>{
+//     if(Math.random() >= 0.5){
+//         resolve("you won lottery")
+//     } else {
+//         reject('you loose lottery')
+//     }
+// })
+// lotteryTicket.then(res=>console.log(res)).catch(err=>console.log(err))
+
+
+// promisifying =>convert the callback based funtions to promise based functions
+// eg
+
+// wait(2).then(response=>console.log('2 sec wait'))
+
+// const wait = function(seconds){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(resolve,seconds*1000)
+//     })
+// }
+
+// chaining multiple setTimeout functions
+// wait(1)
+//     .then(()=>{
+//     console.log('1 sec')
+//     return wait(1)
+// }).then(()=>{
+//     console.log('2 sec');
+//     return wait(1)
+// }).then(()=>{
+//     console.log('3 sec');
+//     return wait(1)
+// }).then(()=>{
+//     console.log('4 sec');
+// })
+
+
+// async await 
+// const myFun = async function(){
+//     const res = await fetch('url_here')
+//     const data = await res.json()
+//     console.log(data);
+// }
+
+// lets create whereAmI function using async await
+// also handle error using try-catch block
+
+const whereAmI1 = async function(lat,lng){
+    try{
+
+        // get lat lng
+        const resGeo = await fetch(`https://geokeo.com/geocode/v1/reverse.php?lat=${lat}&lng=${lng}&api=19ccc8b8dcb5cc7bee166db8e6bee778`)
+        
+        if(!resGeo.ok){
+            throw new Error(`Something went wrong resGeo ${resGeo.status}`)
+        }
+       
+        const dataGeo = await resGeo.json()
+        if(dataGeo.status !== "ZERO_RESULTS"){
+            const {country,district} = dataGeo.results[0].address_components
+            console.log(country,district);
+
+            // country
+            const response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+            if(!response.ok){
+                throw new Error('Something went wrong resCountry')
+            }
+            const data = await response.json()
+            console.log(data)
+            renderCountry(data[0])
+
+            // neighbours
+            const neighbours = data[0].borders[0]
+            
+            const neighboursResponse = await fetch(`https://restcountries.com/v3.1/alpha/${neighbours}`)
+            if(!neighboursResponse.ok){
+                throw new Error('Something went wrong resNeighbours')
+            }
+            const neighboursResponseData = await neighboursResponse.json()
+            renderCountry(neighboursResponseData[0])
+        } else {
+            throw new Error('lat,lng not found')
+        }
+        
+    } catch(err){
+        // console.log(err.message)
+        renderError(err.message)
+    }
+}
+
+btn.addEventListener('click',function(){
+    whereAmI1(52.508, 13.381)
+    // whereAmI1(-3.933, 8.474)
+}) 
